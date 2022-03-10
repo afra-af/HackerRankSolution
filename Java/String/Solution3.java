82d9ced242d8c2d25e0d9417a7f880bf5c64202a
AFRA <81957946+afra-af@users.noreply.github.com>
2022-03-10 18:59:34 +0530

    Create Solution3.java

---
+++ Java/String/Solution3.java
@@ -0,0 +1,27 @@
+/*A palindrome is a word, phrase, number, or other sequence of characters which reads the same backward or forward.
+
+Given a string , print Yes if it is a palindrome, print No otherwise.
+
+Constraints
+
+ will consist at most  lower case english letters.
+Sample Input
+
+madam
+Sample Output
+
+Yes*/
+
+import java.io.*;
+import java.util.*;
+
+public class Solution3 {
+
+    public static void main(String[] args) {
+        
+        Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+        /* Enter your code here. Print output to STDOUT. */
+        System.out.println(A.equals(new StringBuilder(A).reverse().toString())? "Yes":"No");
+    }
+}
